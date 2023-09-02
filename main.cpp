61c6913f62aa7d6767f5137941f3be45a24b9d5a
Jacob <78283092+Dunn-Dev8@users.noreply.github.com>
2023-09-03 07:29:55 +0930

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,10 @@
-#include <gtk/gtk.h>
+#include <gtkmm.h>
 
-static void activate (GtkApplication *app, gpointer user_data)
-{
-  GtkWidget *window;
-  GtkWidget *button;
-  //Declare window and set properties 
-  window = gtk_application_window_new (app);
-  gtk_window_set_title (GTK_WINDOW (window), "Gtk Boilerplate App");
-  gtk_window_set_default_size (GTK_WINDOW (window), 500,700);
+int main(int argc, char* argv[]) {
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
 
-  gtk_window_present (GTK_WINDOW (window));
-}
+    Gtk::Window window;
+    window.set_default_size(200, 200);
 
-int main (int argc, char **argv) {
-  //main app
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new ("org.dunn.gtkdemoapp", G_APPLICATION_DEFAULT_FLAGS);
-  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-  status = g_application_run (G_APPLICATION (app), argc, argv);
-  g_object_unref (app);
-
-  return status;
+    return app->run(window);
 }
